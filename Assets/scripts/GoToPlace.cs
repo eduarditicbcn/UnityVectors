c8e931667b9fd31eb4cadb99ef7aad0024e0beb0
edu1010 <eduard.corral@iticbcn.cat>
2026-02-22 20:32:58 +0100

    normalize

--- Assets/scripts/GoToPlace.cs
+++ Assets/scripts/GoToPlace.cs
@@ -9,6 +9,7 @@ public class GoToPlace : MonoBehaviour
         //Primer calculem la direcció
         //la direcció es el lloc al que volem anar - la nostra posició actual
         Vector3 dir = TargetPos.position - transform.position;
+        dir = dir.normalized;//fem que el vector direcció valgui 1 això s'anomena el modul del vector, i ens permet multiplicar per una velocitat després i així fer que es mogui sempre igual.
         //actualitzem la posició del objecte afectat
         transform.position = transform.position + dir * Velocity *Time.deltaTime;
         //time.deltateime es el temps entre fotogrames, multiplicar per aquest  factor ens permet que doni igual el framerate, sempre trigui el mateix
